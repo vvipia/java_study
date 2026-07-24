753578befbb387f8b5eb6382e9c6a5107378c27e
GIoriousPurpose <vvipia@naver.com>
2026-07-24 10:25:30 +0900

    feat: object-oriented programming

---
+++ java_basic/3) object-oriented programming/3-1) rectangular program to object-oriented/src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    int width;
+    int height;
+    int calculateArea() {
+        return width * height;
+    }
+    int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+    boolean isSquare() {
+        return this.width == this.height;
+    }
+}
---
+++ java_basic/3) object-oriented programming/3-2) Object Oriented Account/src/Account.java
@@ -0,0 +1,13 @@
+class Account {
+    int balance; // 잔액
+    void deposit(int amount) {
+        balance += amount;
+    }
+    void withdraw(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("잔액 부족");
+        }
+    }
+}
